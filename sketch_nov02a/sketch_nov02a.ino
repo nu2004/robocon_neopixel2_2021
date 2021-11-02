dfdd907e55092797b7b2025556d0252f0b28128d
N <83013717+numao2004@users.noreply.github.com>
2021-11-02 20:46:24 +0900

    Create sketch_nov02a.ino

---
+++ sketch_nov02a/sketch_nov02a.ino
@@ -0,0 +1,35 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_COUNT 300
+#define PIN1        2
+#define PIN2        3
+#define PIN3        4
+
+Adafruit_NeoPixel pixels(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
+int step_num = 0;
+int delta = 1;
+
+int color[] = {255, 255, 0};
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+  if(digitalRead(PIN1) == 1) {
+    if(digitalRead(PIN1) == 1) {
+      if(digitalRead(PIN1) == 1) {
+    
+      } else {
+      
+      }
+    } else {
+      
+    }
+  } else {
+    
+  }
+  
+  
+
+}
