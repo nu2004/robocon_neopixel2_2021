1ecfa78349f74ce80cec4a1b10a8d9bfe2b767c4
N <83013717+numao2004@users.noreply.github.com>
2021-11-05 17:20:17 +0900

    Update sketch_nov02a.ino

--- sketch_nov02a/sketch_nov02a.ino
+++ sketch_nov02a/sketch_nov02a.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_COUNT 300
+#define PIN         
 #define PIN1        2
 #define PIN2        3
 #define PIN3        4
@@ -17,19 +18,34 @@ void setup() {
 
 
 void loop() {
+  pixels.clear;
   if(digitalRead(PIN1) == 1) {
-    
-    if(digitalRead(PIN1) == 1){
-    
-      if(digitalRead(PIN1) == 1) {
-        
-        if(digitalRead(PIN1) == 1) {
-
-        }else{
-          
-        }
-
-  
-  
-
-}
+    if(digitalRead(PIN2) == 1){   
+      if(digitalRead(PIN3) == 1) {
+      pixels.setPixelColor(0,pixels.Color(30,0,0));
+      }else{
+      pixels.setPixelColor(0,pixels.Color(30,0,0)); 
+      }
+    }else{   
+      if(digitalRead(PIN3) == 1) {
+      pixels.setPixelColor(0,pixels.Color(30,0,0)); 
+      }else{
+      pixels.setPixelColor(0,pixels.Color(30,0,0)); 
+      }
+    }
+  }else{
+    if(digitalRead(PIN2) == 1){
+      if(digitalRead(PIN3) == 1) {
+      pixels.setPixelColor(0,pixels.Color(30,0,0));  
+      }else{
+      pixels.setPixelColor(0,pixels.Color(30,0,0));   
+      }
+    }else{    
+      if(digitalRead(PIN3) == 1) {
+      pixels.setPixelColor(0,pixels.Color(30,0,0));   
+      }else{
+      pixels.setPixelColor(0,pixels.Color(30,0,0));   
+      }
+    }
+  }
+  pixels.show;
